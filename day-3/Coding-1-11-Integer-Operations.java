0a555624a81416a3f51846577c8d273a89ad581b
aquibajani <aquib.ajani@obsidi.com>
2026-03-11 11:15:00 -0400

    day 3 1-11 integer operations

--- day-3/Coding-1-11-Integer-Operations.java
+++ day-3/Coding-1-11-Integer-Operations.java
@@ -4,18 +4,17 @@ public class IntegerOperations {
 
 	public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int userInput = 0, positiveCount = 0, negativeCount = 0, evenCount = 0, oddCount = 0, total = 0, count = 0;
-    double average = 0.00;
+    int userInput = 0, positiveCount = 0, negativeCount = 0, evenCount = 0, oddCount = 0, count = 0;
+    double average = 0.00, total = 0.00;
 
-    System.out.println("Enter the first integer(0 to terminate): ");
+    System.out.print("Enter the first integer(0 to terminate): ");
     userInput = scanner.nextInt();
 
     if(userInput == 0){
       System.out.println("No numbers are entered except 0.");
     } else {
         while(userInput!=0){
-          System.out.println("Enter the next integer(0 to terminate): ");
-          userInput = scanner.nextInt();
+          
           count++;
           //calculations
           if(userInput > 0){
@@ -32,14 +31,17 @@ public class IntegerOperations {
 
           total = total + userInput;
           average = total / count;
+
+          System.out.print("Enter the next integer(0 to terminate): ");
+          userInput = scanner.nextInt();
         }
 
-         System.out.println("The number of positives is " + positiveCount);
-         System.out.println("The number of negatives is " + negativeCount);
-         System.out.println("The number of evens is " + evenCount);
-         System.out.println("The number of odds is " + oddCount);
-         System.out.println("The total is " + total);
-         System.out.println("The average is " + average);
+         System.out.println("The number of positives is " + positiveCount +".");
+         System.out.println("The number of negatives is " + negativeCount +".");
+         System.out.println("The number of evens is " + evenCount +".");
+         System.out.println("The number of odds is " + oddCount +".");
+         System.out.println("The total is " + (int) total +".");
+         System.out.printf("The average is %.2f.\n", average);
 
       }
 		
