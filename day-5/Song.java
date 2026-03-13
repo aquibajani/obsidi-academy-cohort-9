7dc76eb07810fd9f4784761e04594ac1737d99d2
aquibajani <aquib.ajani@obsidi.com>
2026-03-13 11:39:31 -0400

    day 5 code and whiteboard

---
+++ day-5/Song.java
@@ -0,0 +1,18 @@
+public class Song {
+
+  // Step 1: define a new method named chorus
+  public static void chorus() {
+      System.out.println("I'm looking over a four-leaf clover");
+      System.out.println("That I overlooked before");
+  }
+
+  public static void main(String args[]) {
+    chorus();
+    System.out.println("One leaf is sunshine, the second is rain");
+    System.out.println("Third is the roses that grow in the lane");
+    System.out.println();
+    System.out.println("No need explaining, the one remaining");
+    System.out.println("Is somebody I adore");
+    chorus();
+  }
+}
