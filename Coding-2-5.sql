9d9495aecd50ec149c8254050aa17a24d10874d2
aquibajani <aquib.ajani@obsidi.com>
2026-04-01 09:50:28 -0400

    Add files via upload

---
+++ Coding-2-5.sql
@@ -0,0 +1,66 @@
+-- Retrieve All User IDs: Select all unique userId values from the "User" table
+SELECT DISTINCT	"userId" FROM "User";
+
+-- Profiles in Canada: Select headline, city, and country for profiles located in 'Canada'
+SELECT "headline", "city", "country"
+FROM "Profile" WHERE COUNTRY = 'Canada'
+
+-- INNER JOIN User and Profile to display firstName, lastName, and headline
+SELECT "User"."firstName", "User"."lastName", "Profile"."headline" FROM "User"
+INNER JOIN "Profile" ON "User"."userId" = "Profile"."userId";
+
+-- Users Without Profiles (Outer Join): LEFT JOIN to include all users, ORDER BY headline 
+SELECT
+	"User"."userId",
+	"User"."firstName",
+	"User"."lastName",
+	"User"."username",
+	"User"."phone",
+	"User"."emailId",
+	"User"."emailVerified",
+	"Profile"."headline"
+FROM
+	"User"
+	LEFT JOIN "Profile" ON "User"."userId" = "Profile"."userId"
+ORDER BY
+	"Profile"."headline";
+
+-- GROUP BY emailVerified status and COUNT the number of users in each group
+SELECT
+	"emailVerified",
+	COUNT(*) AS "user_count"
+FROM
+	"User"
+GROUP BY
+	"emailVerified"
+
+-- Use UNION to combine userId from all profiles AND users in Canada
+SELECT
+	"userId"
+FROM
+	"Profile"
+WHERE
+	"country" = 'Canada'
+UNION
+SELECT
+	"userId"
+FROM
+	"User"
+WHERE
+	"userId" IN (
+		SELECT
+			"userId"
+		FROM
+			"Profile"
+		WHERE
+			"country" = 'Canada'
+	)
+
+-- Which ACID property is ensured by the UNIQUE constraint on username?
+
+-- Write a TRIGGER function named log_profile_update BEFORE UPDATE on the Profile table
+CREATE OR REPLACE FUNCTION LOG_PROFILE_UPDATE ()
+RETURNS TRIGGER AS
+	$$ BEGIN -- Log the update action here (e.g., insert into a log table) RETURN NEW; END; $$ LANGUAGE PLPGSQL;
+	CREATE TRIGGER PROFILE_UPDATE_TRIGGER BEFORE UPDATE ON PROFILE
+	FOR EACH ROW EXECUTE FUNCTION LOG_PROFILE_UPDATE ();
